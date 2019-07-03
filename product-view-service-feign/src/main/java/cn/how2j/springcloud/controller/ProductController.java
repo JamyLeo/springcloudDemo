b42810b789c87d8e205e0049e15e87faba1bb104
JamyLeo <1096166529@qq.com>
2019-07-03 17:40:10 +0800

    2019-7-3

--- product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
@@ -3,6 +3,7 @@ package cn.how2j.springcloud.controller;
 import cn.how2j.springcloud.domain.Product;
 import cn.how2j.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,14 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
+    @Value("${version}")
+    String version;
 
     @RequestMapping("/products")
     public Object products(Model model) {
         List<Product> ps = productService.listProducts();
         model.addAttribute("ps",ps);
+        model.addAttribute("version",version);
         return "products";
     }
 }
